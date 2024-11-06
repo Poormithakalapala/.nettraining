9c470a24dbeafd665fc6e624a63a91ca1105e549
Poormitha Kalapala <poormithak@infinite.com>
2024-11-06 11:16:03 +0530

    ASSESSMENTS csharp

---
+++ csharp/ASSESSMENTS/ASSESSMENT1/ASSESSMENT1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSESSMENT1
+{
+    class Program
+    {
+        static void LargestNum()
+        {
+            Console.WriteLine("Enter num1:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter num2:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter num3:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 >= num2 && num1 >= num3)
+            {
+                Console.WriteLine(num1);
+            }
+            else if ( num2>=num1 && num2 >= num3)
+            {
+                Console.WriteLine(num2);
+            }
+            else
+            {
+                Console.WriteLine(num3);
+            }
+
+        }
+        static void RemoveCharAtPosition()
+        {
+            Console.WriteLine("Enter a string:");
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine("Enter the position to remove:");
+            int position = Convert.ToInt32(Console.ReadLine());
+            if (position >= 0 && position < inputString.Length)
+            {
+                string result = inputString.Substring(0, position) + inputString.Substring(position + 1);
+                Console.WriteLine($"String after removing character at position {position}: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid position. Position should be within the string length.");
+            }
+        }
+
+        static void SwapFirstLast()
+        {
+            Console.WriteLine("Enter a string:");
+            string data = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(data) || data.Length == 1)
+            {
+                Console.WriteLine($"No change: {data}");
+            }
+            else
+            {
+
+                char firstChar = data[0];
+                char lastChar = data[data.Length - 1];
+                string swappedString = lastChar + data.Substring(1, data.Length - 2) + firstChar;
+
+                Console.WriteLine($"Swapped string: {swappedString}");
+            }
+        
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Largest Numbers--");
+            LargestNum();
+
+            Console.WriteLine("---Remove Character at Position---");
+            RemoveCharAtPosition();
+            Console.WriteLine("---SwapString---");
+            SwapFirstLast();
+
+            Console.Read();
+        }
+    }
+}
+    
+
+        
---
+++ csharp/ASSESSMENTS/ASSESSMENT1/ASSESSMENT1/position.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSESSMENT1
+{
+    class position
+    {
+        public static string RemoveCharAt(string str, int index)
+        {
+            if (index < 0 || index >= str.Length)
+            {
+                Console.WriteLine("Index out of range");
+                return str;
+            }
+            return str.Remove(index, 1);
+        }
+        static string ExchangeFirstAndLast(string str)
+        {
+            if (string.IsNullOrEmpty(str) || str.Length == 1)
+            {
+                return str;
+            }
+            char firstChar = str[0];
+            char lastChar = str[str.Length - 1];
+            string newStr = lastChar + str.Substring(1, str.Length - 2) + firstChar;
+            return newStr;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveCharAt("python", 1));
+            Console.WriteLine(RemoveCharAt("python", 0));
+            Console.WriteLine(RemoveCharAt("python", 3));
+            Console.WriteLine(RemoveCharAt("python", 4));
+            Console.WriteLine("========Exchange First and last character=======");
+            string input1 = "abcd";
+            string input2 = "xy";
+            Console.WriteLine(ExchangeFirstAndLast(input1));
+            Console.WriteLine(ExchangeFirstAndLast(input2));
+        }
+    }
+}
