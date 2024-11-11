1bbb7ca1ee41ddca8ab581b7beef29a0cdabeed1
Poormitha Kalapala <poormithak@infinite.com>
2024-11-11 07:45:56 +0530

    assignment4 csharp

---
+++ csharp/ASSIGNMENTS/assignment4/assignment4/Doctor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class Book
+    {
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public Book(string bookName, string authorName)
+        {
+            BookName = bookName;
+            AuthorName = authorName;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Book Name: {BookName}, Author: {AuthorName}");
+        }
+    }
+    public class BookShelf
+    {
+        Book[] books = new Book[5];
+        public Book this[int index]
+        {
+            get { return books[index]; }
+            set { books[index] = value; }
+        }
+        public void DisplayAllBooks()
+        {
+            for (int i = 0; i < books.Length; i++)
+            {
+                if (books[i] != null)
+                {
+                    books[i].Display();
+                }
+            }
+        }
+    }
+
+    public class DoctorBook
+    {
+        public static void Main(string[] args)
+        {
+            BookShelf shelf = new BookShelf();
+            shelf[0] = new Book("Mental Health", "KVR");
+            shelf[1] = new Book("Science", "GVK");
+            shelf[2] = new Book("YOGA", "Geetha");
+            shelf.DisplayAllBooks();
+            Console.Read();
+        }
+    }
+}
+
---
+++ csharp/ASSIGNMENTS/assignment4/assignment4/Scholarship.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class Scholarship
+    {
+        public double Merit(int marks, double fees)
+        {
+            double scholarshipAmount = 0.0;
+
+            if (marks >= 70 && marks <= 80)
+            {
+                scholarshipAmount = 0.20 * fees;
+            }
+            else if (marks > 80 && marks <= 90)
+            {
+                scholarshipAmount = 0.30 * fees;
+            }
+            else if (marks > 90)
+            {
+                scholarshipAmount = 0.50 * fees;
+            }
+
+            return scholarshipAmount;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter your marks: ");
+            int marks = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your fees: ");
+            double fees = Convert.ToDouble(Console.ReadLine());
+            Scholarship a = new Scholarship();
+            double scholarshipAmount = a.Merit(marks, fees);
+            Console.WriteLine("Scholarship Amount: " + scholarshipAmount);
+            Console.ReadLine();
+        }
+    }
+}
