cb3b919d8689320ea024e064e00ed7544558e86d
Poormitha Kalapala <poormithak@infinite.com>
2024-11-14 17:26:22 +0530

    Assignment5 csharp

---
+++ csharp/ASSIGNMENTS/Assignment5/Assignment5/Books.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    public class Books
+    {
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+
+        public Books(string bookName, string authorName)
+        {
+            BookName = bookName;
+            AuthorName = authorName;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Book Name: {BookName}, Author Name: {AuthorName}");
+        }
+    }
+
+    public class BookShelf
+    {
+        private Books[] bookArray = new Books[5];
+
+        public Books this[int index]
+        {
+            get { return bookArray[index]; }
+            set { bookArray[index] = value; }
+        }
+
+        public void DisplayBooks()
+        {
+            foreach (var book in bookArray)
+            {
+                book?.Display();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            BookShelf shelf = new BookShelf();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter the name of Book {i + 1}: ");
+                string bookName = Console.ReadLine();
+
+                Console.Write($"Enter the author of Book {i + 1}: ");
+                string authorName = Console.ReadLine();
+
+                shelf[i] = new Books(bookName, authorName);
+            }
+
+            Console.WriteLine("\nDisplaying all books:");
+            shelf.DisplayBooks();
+            Console.Read();
+        }
+        
+    }
+}
---
+++ csharp/ASSIGNMENTS/Assignment5/Assignment5/WriteFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment5
+{
+    class WriteFile
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the number of lines you want to write to the file: ");
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+
+            string[] lines = new string[numberOfLines];
+
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                Console.Write($"Enter line {i + 1}: ");
+                lines[i] = Console.ReadLine();
+            }
+
+
+            string filePath = "user_output.txt";
+
+            try
+            {
+                File.WriteAllLines(filePath, lines);
+                Console.WriteLine("Lines written to the file successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
