fa802d4efc45cca451a23e79921375d7fd8cd969
Poormitha Kalapala <poormithak@infinite.com>
2024-12-11 12:58:48 +0530

    SQL

--- SQL/ASSESSMENTS/assessment3/question3ADO/question3ADO/Program.cs
+++ SQL/ASSESSMENTS/assessment3/question3ADO/question3ADO/Program.cs
@@ -1,81 +1,92 @@
 ﻿using System;
-using System.Data;
-using System.Data.SqlClient;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
-namespace question3ADO
+namespace Question3ADO
+
 {
-    using System;
-    using System.Data;
-    using System.Data.SqlClient;
+    class Program
 
-    namespace question3ADO
     {
-        class Program
+
+        static void Main(string[] args)
+
         {
-            static void Main(string[] args)
+
+            string connectionString = "Server = ICS-LT-8ZFLQ73\\SQLEXPRESS; Database = master; Trusted_Connection = True;";
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+
             {
 
-                string connectionString = "Server=ICS-LT-8ZFLQ73\\SQLEXPRESS;Database=master;Trusted_Connection=True";
+                connection.Open();
 
-                
-                using (SqlConnection conn = new SqlConnection(connectionString))
+
+
+                using (SqlCommand command = new SqlCommand("InsertProductDetails", connection))
+
                 {
-                    try
+
+                    command.CommandType = CommandType.StoredProcedure;
+
+
+
+                    command.Parameters.AddWithValue("@ProductName", "Shoes");
+
+                    command.Parameters.AddWithValue("@Price", 10000);
+
+
+
+                    SqlParameter productIdParam = new SqlParameter("@GeneratedProductId", SqlDbType.Int)
+
                     {
-                        
-                        conn.Open();
 
-                        // Create a command to execute the stored procedure
-                        using (SqlCommand cmd = new SqlCommand("Insert_ProdDetai", conn))  // Make sure the name matches the stored procedure
-                        {
-                            cmd.CommandType = CommandType.StoredProcedure;
+                        Direction = ParameterDirection.Output
 
-                            // Add parameters for the stored procedure
-                            cmd.Parameters.AddWithValue("@ProductName", "Shoes");
-                            cmd.Parameters.AddWithValue("@Price", 10000);
+                    };
 
-                            // Define the output parameter for GeneratedProductId
-                            SqlParameter productIdParam = new SqlParameter("@GeneratedProductId", SqlDbType.Int)
-                            {
-                                Direction = ParameterDirection.Output
-                            };
-                            cmd.Parameters.Add(productIdParam);
+                    command.Parameters.Add(productIdParam);
 
-                            // Execute the stored procedure
-                            cmd.ExecuteNonQuery();
+                    SqlParameter discountedPriceParam = new SqlParameter("@DiscountedPrice", SqlDbType.Decimal)
 
-                            // Retrieve the output value for GeneratedProductId
-                            int generatedProductId = (int)productIdParam.Value;
-
-                            // Output the generated ProductId
-                            Console.WriteLine($"Generated ProductId: {generatedProductId}");
-
-                            // Retrieve the DiscountedPrice (calculated in the database) by ProductId
-                            using (SqlCommand getDiscountedPriceCmd = new SqlCommand("SELECT DiscountedPrice FROM Products_Detail WHERE ProductId = @GeneratedProductId", conn))
-                            {
-                                getDiscountedPriceCmd.Parameters.AddWithValue("@GeneratedProductId", generatedProductId);
-                                decimal discountedPrice = (decimal)getDiscountedPriceCmd.ExecuteScalar();
-
-                                // Output the DiscountedPrice
-                                Console.WriteLine($"Discounted Price: {discountedPrice}");
-                            }
-
-                            // Wait for user input before closing the console window
-                            Console.WriteLine("Press Enter to exit...");
-                            Console.ReadLine();
-                        }
-                    }
-                    catch (Exception ex)
                     {
-                        // Handle any exceptions that occur
-                        Console.WriteLine($"Error: {ex.Message}");
-                    }
+
+                        Direction = ParameterDirection.Output
+
+                    };
+
+                    command.Parameters.Add(discountedPriceParam);
+
+
+
+                    command.ExecuteNonQuery();
+
+
+
+                    int generatedProductId = (int)productIdParam.Value;
+
+                    decimal discountedPrice = (decimal)discountedPriceParam.Value;
+
+
+
+                    Console.WriteLine("Generated Product ID: " + generatedProductId);
+
+                    Console.WriteLine("Discounted Price: " + discountedPrice);
+
+                    Console.Read();
+
                 }
+
             }
+
         }
+
     }
 
 }
+
